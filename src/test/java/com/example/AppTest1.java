6ed9953a93fbbb2b07c2bb6d49923e6078f80402
Luis Rendon Suarez <129043244+1u1sRR0@users.noreply.github.com>
2025-10-01 23:31:16 +0200

    Create AppTest1.java

---
+++ src/test/java/com/example/AppTest1.java
@@ -0,0 +1,6 @@
+package com.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class AppTest {
+  @Test void adds(){ assertEquals(3, new App().add(1,2)); }
+}
